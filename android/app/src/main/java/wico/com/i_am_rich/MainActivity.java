1b5ab7160261d89b9dd31329a1a00ec37809ef56
Wico Pretorius <wico@jager.co.za>
2021-06-27 09:26:40 +0200

    first commit

---
+++ android/app/src/main/java/wico/com/i_am_rich/MainActivity.java
@@ -0,0 +1,6 @@
+package wico.com.i_am_rich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
+      ),
+    ),
+  );
+}
